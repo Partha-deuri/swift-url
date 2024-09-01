3942da356995dff9fa652b18650caece81594fa7
Parthapratim Deuri <130203363+Partha-deuri@users.noreply.github.com>
2024-09-01 19:35:49 +0530

    Update Url.js

--- api/Url.js
+++ api/Url.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const UrlSchema = new mongoose.Schema({
     longUrl: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     shortUrl: {
         type: String,
