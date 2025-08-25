860c24cefadbee9d84fd0adfc4bb7a1658f6c74c
Parthapratim Deuri <130203363+Partha-deuri@users.noreply.github.com>
2025-08-26 00:04:45 +0530

    Update func.js

--- front/func.js
+++ front/func.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
         res
             .then((response) => response.json())
             .then(json => {
-                shortUrl = window.location.href+'/q?'+ json.shortUrl;
+                shortUrl = await (window.location.hostname+'/q?'+ json.shortUrl);
                 shortUrlBox.innerText = shortUrl;
                 gotoBtn.href = shortUrl;
                 qrCode.src = 'https://quickchart.io/qr?text=' + shortUrl;
